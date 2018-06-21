e07e7536b5197903ea403a3ae6e957e9641ec501
Ben Gundersen <ben@bengundersen.com>
2018-06-20 20:34:07 -0400

    Finally found a decent image zoom

--- src/screens/HotelMapScreen.js
+++ src/screens/HotelMapScreen.js
@@ -7,8 +7,6 @@ import {
   Dimensions,
   Image,
   MapView,
-  PanResponder,
-  ScrollView,
   StyleSheet,
   Text,
   TouchableHighlight,
@@ -16,6 +14,7 @@ import {
 } from 'react-native';
 
 import globalStyles from '../globalStyles';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 let window = Dimensions.get('window');
 
@@ -26,97 +25,17 @@ export default class HotelMapScreen extends React.Component {
     title: "Hotel Map"
   };
 
-  constructor() {
-    super();
-    this.state = {
-      scale: 1,
-      mapX: -global.Store.getDimension('HOTEL_MAP_WIDTH') / 4,
-      mapY: -global.Store.getDimension('HOTEL_MAP_HEIGHT') / 4
-    }
-  }
-
-  onMapZoomIn() {
-    let newScale = this.scale * 1.2;
-    if (newScale > 2) {
-      newScale = 2;
-    }
-    this.setState({
-      scale: newScale
-    });
-  }
-
-  onMapZoomOut() {
-    let newScale = this.scale * 0.8;
-    if (newScale < 0.5) {
-      newScale = 0.5;
-    }
-    this.setState({
-      scale: newScale
-    });
-  }
-
-  componentWillMount() {
-    this._lastLeft = this.state.mapX;
-    this._lastTop  = this.state.mapY;
-
-    this._panResponder = PanResponder.create({
-      // Ask to be the responder:
-      onStartShouldSetPanResponder: (evt, gestureState) => false,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) => false,
-      onMoveShouldSetPanResponder: (evt, gestureState) => true,
-      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-
-      onPanResponderGrant: (evt, gestureState) => {
-        // The guesture has started. Show visual feedback so the user knows
-        // what is happening!
-
-        // gestureState.{x,y}0 will be set to zero now
-      },
-      onPanResponderMove: (evt, gestureState) => {
-        // The most recent move distance is gestureState.move{X,Y}
-        this.setState({
-          mapX: this._lastLeft + gestureState.dx,
-          mapY: this._lastTop  + gestureState.dy
-        });
-        // The accumulated gesture distance since becoming responder is
-        // gestureState.d{x,y}
-      },
-      onPanResponderEnd: (evt, gestureState) => {
-        this._lastLeft += gestureState.dx;
-        this._lastTop  += gestureState.dy;
-      },
-      onPanResponderTerminationRequest: (evt, gestureState) => true,
-      onPanResponderRelease: (evt, gestureState) => {
-        // The user has released all touches while this view is the
-        // responder. This typically means a gesture has succeeded
-      },
-      onPanResponderTerminate: (evt, gestureState) => {
-        // Another component has become the responder, so this gesture
-        // should be cancelled
-      },
-      onShouldBlockNativeResponder: (evt, gestureState) => {
-        // Returns whether this component should block native components from becoming the JS
-        // responder. Returns true by default. Is currently only supported on android.
-        return true;
-      },
-    });
-  }
-
   render() {
     const HOTEL_MAP_WIDTH  = global.Store.getDimension('HOTEL_MAP_WIDTH');
     const HOTEL_MAP_HEIGHT = global.Store.getDimension('HOTEL_MAP_HEIGHT');
     
     return (
-      <ScrollView style={ styles.container }
-        minimumZoomScale={ 0.5 }
-        maximumZoomScale={ 2 }
-        bounces={ false }
-      >
-        <Image
-          style={[ styles.map, { width: HOTEL_MAP_WIDTH, height: HOTEL_MAP_HEIGHT, left: this.state.mapX, top: this.state.mapY } ]}
-          source={{ uri: global.Store.getImage('HOTEL_MAP') }}
+      <View style={ styles.container }>
+        <ImageViewer
+          renderIndicator={ () => {} }
+          imageUrls={[{ url: global.Store.getImage('HOTEL_MAP') }]}
         />
-      </ScrollView>
+      </View>
     );
   }
 
